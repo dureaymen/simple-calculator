117071a7e6843b24c9e630c27cc9d6961b4cb64a
dureaymen <dureaymen@gmail.com>
2024-05-10 11:33:41 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from 'inquirer';
+let question = await inquirer.prompt([
+    {
+        name: "question1",
+        type: "number",
+        message: "Enter the first number:"
+    },
+    {
+        name: "question2",
+        type: "number",
+        message: "Enter the second number:"
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Select the operator",
+        choices: ["+", "-", "*", "/"]
+    },
+]);
+if (question.operator === "+") {
+    console.log(parseInt(question.question1) + parseInt(question.question2));
+}
+else if (question.operator === "-") {
+    console.log(parseInt(question.question1) - parseInt(question.question2));
+}
+else if (question.operator === "*") {
+    console.log(parseInt(question.question1) * parseInt(question.question2));
+}
+else if (question.operator === "/") {
+    console.log(parseInt(question.question1) / parseInt(question.question2));
+}
+else {
+    console.log("select again operator");
+}
